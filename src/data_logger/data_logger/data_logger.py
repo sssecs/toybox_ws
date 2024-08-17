fda16261b122a727a5e9c43c7f0c6426876f9261
sssecs <ssssssecs@gmail.com>
2024-08-17 11:01:42 +0800

    modify data_logger to record Open3D data

--- src/data_logger/data_logger/data_logger.py
+++ src/data_logger/data_logger/data_logger.py
@@ -1,11 +1,35 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs_py.point_cloud2 as pc2
 import pickle
 from toybox_interfaces.srv import Switch
+import numpy as np
+import open3d
+
+
 
 
 class DataLogger(Node):
+    def convertCloudFromRosToOpen3d(self,ros_cloud):
+        
+        # Get cloud data from ros_cloud
+        field_names=[field.name for field in ros_cloud.fields]
+        cloud_data = list(pc2.read_points(ros_cloud, skip_nans=True, field_names = field_names))
+
+        # Check empty
+        open3d_cloud = open3d.geometry.PointCloud()
+        if len(cloud_data)==0:
+            print("Converting an empty cloud")
+            return None
+
+        # Set open3d_cloud
+        xyz = [(x,y,z) for x,y,z,a,b in cloud_data ] # get xyz
+        open3d_cloud.points = open3d.utility.Vector3dVector(np.array(xyz))
+
+        # return
+        return open3d_cloud
     def __init__(self):
         super().__init__('data_logger')
         self.odom_subscriber = self.create_subscription(
@@ -18,10 +42,13 @@ class DataLogger(Node):
             '/p3d/odom_p3d',
             self.p3d_subscriber_callback,
             10)
-        self.timer = self.create_timer(0.2, self.timer_callback)
+        self.pc2_subscriber = self.create_subscription(PointCloud2, '/toybox/pc2', 
+                                                      self.pc2_subscriber_callback, 10  )
+        
         self.record_switch = self.create_service(Switch, 'record_switch', self.switch_callback)
         self.odom_data = Odometry
         self.p3d_data = Odometry
+        self.pc2_data = PointCloud2
         self.record_switch = False
 
     def odom_subscriber_callback(self, msg):
@@ -30,11 +57,16 @@ class DataLogger(Node):
     def p3d_subscriber_callback(self, msg):
         self.p3d_data = msg
 
-    def timer_callback(self):
+    def pc2_subscriber_callback(self, msg):
+        self.pc2_data = msg
+        self.record()
+
+    def record(self):
         if self.record_switch:
+            open_3d_data = self.convertCloudFromRosToOpen3d(self.pc2_data)
             try:
                 with open("/home/sykes/data.pickle", "wb") as f:
-                    pickle.dump((self.p3d_data,self.odom_data), f, protocol=pickle.HIGHEST_PROTOCOL)
+                    pickle.dump((self.p3d_data,self.odom_data,open_3d_data), f, protocol=pickle.HIGHEST_PROTOCOL)
             except Exception as ex:
                 print("Error during pickling object (Possibly unsupported):", ex)
 
